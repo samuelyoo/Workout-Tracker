63ed59be998e0803355e90ebd15e57ec1d3756b7
Samuel Yoo <samuel.hc.yoo@forso.net>
2020-07-27 23:13:07 -0400

    move router to router.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
 
 const PORT = process.env.PORT || 8080;
 
-const db = require ("./models")
-
+// const db = require ("./models")
+const router = require("./controllers/router")
 const app = express();
 
 app.use(logger("dev"));
@@ -20,74 +19,12 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true, 
     useFindAndModify: false,
     useUnifiedTopology: true
-  });
-
-// Routes
-
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname + "public/index.html"));
 });
 
-//render exercise html page with this route so user can see that page when he or she clicks "new workout"
-app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/exercise.html"));
-  });
-  
-  //grab last workout
-  app.get("/api/workouts", (req, res) => {
-    db.Workout.find({}).then(data => {
-      // console.log(data)
-      res.json(data)
-    })
-  
-  })
-  
-  //route to update
-  app.put("/api/workouts/:id", (req, res) => {
-    console.log(req.body);
-    db.Workout.findByIdAndUpdate(
-    req.params.id, 
-    {
-        $push: {
-        exercises: req.body
-    }
-    }
-    ).then(data => {
-      console.log(data);
-      res.json(data)
-    });
-  })
-  
-  //route to create to post aka create an exercise
-  app.post("/api/workouts", ({
-    body
-  }, res) => {
-    // console.log(body);
-    db.Workout.create(body).then(data => {
-      //dostuff
-      res.json(data)
-    })
-  
-  })
-  
-  
-  //render stats html page with this route so user can see that page when he or she clicks "new workout"
-  app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/stats.html"));
-  });
-  
-  //this route display 
-  app.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({})
-      // .populate("Workout")
-      .then(data => {
-        res.json(data);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  
+router(app)
+
+// // Routes
+
   
   app.listen(PORT, () => {
     console.log(`App running on port http://localhost:${PORT}`);
